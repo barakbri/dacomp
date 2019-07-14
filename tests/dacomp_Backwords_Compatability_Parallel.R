e58557f0a52b28ebe7f6a712d6f0afbb4e51b928
barakbri <barak_b@yahoo.com>
2019-07-14 21:25:00 +0300

    Update dacomp_Backwords_Compatability_Parallel.R
    add tests for ratio based normalization

--- tests/dacomp_Backwords_Compatability_Parallel.R
+++ tests/dacomp_Backwords_Compatability_Parallel.R
@@ -17,6 +17,12 @@ if(RUN_REGRESSION_TEST_PAPER){
     mean_FDR_BH = 0
     mean_TP_DSFDR = 0
     mean_FDR_DSFDR = 0
+    
+    mean_TP_BH_ratio = 0
+    mean_FDR_BH_ratio = 0
+    mean_TP_DSFDR_ratio = 0
+    mean_FDR_DSFDR_ratio = 0
+    
     for(b in 1:nr.reps){
       if(verbose){
         cat(paste0('Running scenario ID ',scenario_ID,', rep: ',b,' / ',nr.reps,'\n\r'))
@@ -40,14 +46,19 @@ if(RUN_REGRESSION_TEST_PAPER){
                                 y = data$Y,
                                 ind_reference_taxa = result.selected.references$selected_references,
                                 test = DACOMP.TEST.NAME.WILCOXON,
-                                verbose = F,q = q_DSFDR,nr_perm = 10000)
+                                verbose = F,q = q_DSFDR,nr_perm = 10000,compute_ratio_normalization = T)
       
       rejected_BH = which(p.adjust(result.test$p.values.test,method = 'BH')<=q_BH)
       rejected_DSFDR = result.test$dsfdr_rejected
+      rejected_BH_ratio = which(p.adjust(result.test$p.values.test.ratio.normalization,method = 'BH')<=q_BH)
+      rejected_DSFDR_ratio = result.test$dsfdr_rejected_ratio_normalization
       
       
       TP = sum((rejected_BH %in% data$select_diff_abundant))
       TP_DSFDR = sum((rejected_DSFDR %in% data$select_diff_abundant))
+      
+      TP_ratio = sum((rejected_BH_ratio %in% data$select_diff_abundant))
+      TP_DSFDR_ratio = sum((rejected_DSFDR_ratio %in% data$select_diff_abundant))
       
       FDR = ifelse(length(rejected_BH)>0,
                    sum(!(rejected_BH %in% data$select_diff_abundant))/length(rejected_BH),
@@ -56,16 +67,34 @@ if(RUN_REGRESSION_TEST_PAPER){
                          sum(!(rejected_DSFDR %in% data$select_diff_abundant))/length(rejected_DSFDR),
                          0)
       
+      FDR_ratio = ifelse(length(rejected_BH_ratio)>0,
+                   sum(!(rejected_BH_ratio %in% data$select_diff_abundant))/length(rejected_BH_ratio),
+                   0)
+      FDR_DSFDR_ratio = ifelse(length(rejected_DSFDR_ratio)>0,
+                         sum(!(rejected_DSFDR_ratio %in% data$select_diff_abundant))/length(rejected_DSFDR_ratio),
+                         0)
+      
       mean_TP_BH = mean_TP_BH + TP
       mean_FDR_BH = mean_FDR_BH + FDR
       mean_TP_DSFDR = mean_TP_DSFDR + TP_DSFDR
       mean_FDR_DSFDR = mean_FDR_DSFDR + FDR_DSFDR
+      
+      mean_TP_BH_ratio = mean_TP_BH_ratio + TP_ratio
+      mean_FDR_BH_ratio = mean_FDR_BH_ratio + FDR_ratio
+      mean_TP_DSFDR_ratio = mean_TP_DSFDR_ratio + TP_DSFDR_ratio
+      mean_FDR_DSFDR_ratio = mean_FDR_DSFDR_ratio + FDR_DSFDR_ratio
+      
+      
       #cat(paste0('True positives: ',TP,', FDR: ',round(FDR,2),'\n\r'))
     }
     return(list(mean_TP_BH = mean_TP_BH/nr.reps,
                 mean_FDR_BH = mean_FDR_BH/nr.reps,
                 mean_TP_DSFDR = mean_TP_DSFDR/nr.reps,
-                mean_FDR_DSFDR = mean_FDR_DSFDR/nr.reps))
+                mean_FDR_DSFDR = mean_FDR_DSFDR/nr.reps,
+                mean_TP_BH_ratio = mean_TP_BH_ratio/nr.reps,
+                mean_FDR_BH_ratio = mean_FDR_BH_ratio/nr.reps,
+                mean_TP_DSFDR_ratio = mean_TP_DSFDR_ratio/nr.reps,
+                mean_FDR_DSFDR_ratio = mean_FDR_DSFDR_ratio/nr.reps))
   }
   
   
@@ -75,7 +104,7 @@ if(RUN_REGRESSION_TEST_PAPER){
   registerDoParallel(cl)
   start_scenario = 1
   end_scenario = 25
-  nr.reps = 10
+  nr.reps = 1
   scenario_results = foreach(s=start_scenario:end_scenario, .options.RNG=1234) %dorng% { SCENARIO_run_backwards_compatability(s,nr.reps = nr.reps,q_BH = 0.1,q_DSFDR = 0.1) }
   stopCluster(cl)
   result_table = unlist(scenario_results[[1]])
